ba28941a525c842a98ff5090dcf932d0d82d35e6
cocodori <moradulee@gmail.com>
2021-04-04 14:46:46 +0900

    [리액트를 다루는 기술] 9장 컴포넌트 스타일링

---
+++ styling-react/src/CSSModule.js
@@ -0,0 +1,15 @@
+import classNames from "classnames/bind";
+import React from "react";
+import styles from "./CSSModule.module.scss";
+
+const cx = classNames.bind(styles);
+
+const CSSModule = () => {
+  return (
+    <div className={cx("wrapper", "inverted")}>
+      안녕하세요. 저는 <span className="something">CSS Module</span>
+    </div>
+  );
+};
+
+export default CSSModule;
