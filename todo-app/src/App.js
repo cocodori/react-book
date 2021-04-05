e44aa9da816bcc48dbdbc9ae2d9f6dbe4ba0c2dc
cocodori <moradulee@gmail.com>
2021-04-05 17:46:17 +0900

    [리액트를 다루는 기술] 11장 컴포넌트 성능 최적화

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,30 +1,42 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useRef, useState, useReducer } from "react";
 import TodoTemplate from "./TodoTemplate";
 import TodoInsert from "./TodoInsert";
 import TodoList from "./TodoList";
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "리액트 기초 알아보기",
-      checked: true,
-    },
-    {
-      id: 2,
-      text: "컴포넌트 스타일링 해보기",
-      checked: true,
-    },
-    {
-      id: 3,
-      text: "일정 관리 앱 만들어보기",
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case "INSERT":
+      return todos.concat(action.todo);
+    case "REMOVE":
+      return todos.filter((todo) => todo.id !== action.id);
+    case "TOGGLE":
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+}
+
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   //고유 값으로 사용될 id
   //ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
+
   const onInsert = useCallback(
     (text) => {
       const todo = {
@@ -32,7 +44,7 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      dispatch({ type: "INSERT", todo });
       nextId.current += 1;
     },
     [todos]
@@ -40,18 +52,14 @@ const App = () => {
 
   const onRemove = useCallback(
     (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+      dispatch({ type: "REMOVE", id });
     },
     [todos]
   );
 
   const onToggle = useCallback(
     (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo
-        )
-      );
+      dispatch({ type: "TOGGLE", id });
     },
     [todos]
   );
